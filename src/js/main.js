8dc1536f9e48ddf57cc652bc2f6492c063cad2cd
Jeremiah Akpera <jerryakpera@gmail.com>
2023-04-25 14:11:13 +0100

    Latest

--- src/js/main.js
+++ src/js/main.js
@@ -57,26 +57,6 @@ async function getPasswords() {
   toggleLoading();
 }
 
-// Function to display notifications in snackbar
-function displayNotification(msg, color) {
-  // Get the snackbar DIV
-  var snackbar = document.getElementById('snackbar');
-  // Set the content of the notification
-  snackbar.innerText = msg;
-
-  // Modify the color of the snackbar to describe the type of notification
-  // Can be negative, positive or default
-  snackbar.className = color;
-
-  // Add the "show" class to DIV
-  snackbar.className = 'show' + ' ' + color;
-
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(function () {
-    snackbar.className = snackbar.className = '';
-  }, 3000);
-}
-
 // Function to remove the styling from the password boxes after password is copied
 function removeStyling() {
   passwordContentBoxes.forEach((box) => box.classList.remove('copied'));
